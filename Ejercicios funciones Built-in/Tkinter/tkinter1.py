14388d39e2a435a2e3a3be241931801add06215c
José María Vizcaíno <josema.vizcainourban@gmail.com>
2022-11-07 10:16:06 +0100

    Práctica con Tkinter. /n Métodos de posicionamiento grid y pack.

--- Ejercicios funciones Built-in/Tkinter/tkinter1.py
+++ Ejercicios funciones Built-in/Tkinter/tkinter1.py
@@ -1,11 +1,48 @@
 import tkinter
+from tkinter import ttk
 
 window = tkinter.Tk()
 
-label_saludo = tkinter.Label(window,text='¡Hola!', background='yellow',)
-label_saludo.pack(ipadx=50, ipady=50, fill='both')
-label_adios = tkinter.Label(window, text='¡Adios!', background='red')
-label_adios.pack(ipadx=50, ipady=100, fill='both')
+def geometria_grid():
+    ### Login de usuario y contraseña:
+    window.columnconfigure(0,weight=2)
+    window.rowconfigure(1,weight=3)
+    # Usuario
+    username_label = ttk.Label(window, text='Username:')
+    username_label.grid(column=0, row=0,sticky=tkinter.W,padx=5, pady=5)
 
+    username_entry = ttk.Entry(window)
+    username_entry.grid(column=1,row=0,sticky=tkinter.E)
+    # Contraseña
+    password_label = ttk.Label(window, text='Password:')
+    password_label.grid(column=0, row=1,sticky=tkinter.W, padx=5, pady=5)
+
+    password_entry = ttk.Entry(window,show='*')
+    password_entry.grid(column=1,row=1,sticky=tkinter.E)
+    
+    #Boton    
+    login_button = ttk.Button(window, text='Login')
+    login_button.grid(column=1, row=3, sticky=tkinter.E, padx=5, pady=5)
+
+def geometria_pack():
+    label1 = tkinter.Label(window,text='Label1', background='yellow',foreground='blue')
+    label1.pack(ipadx=45, ipady=15)
+
+    label2 = tkinter.Label(window, text='Label2', background='purple',fg='white')
+    label2.pack(ipadx=45, ipady=15)
+
+    label3 = tkinter.Label(window, text='Label3', background='blue',fg='white')
+    label3.pack(ipadx=45, ipady=15)
+
+    label4 = tkinter.Label(window, text='Label4', background='red',fg='white')
+    label4.pack(ipadx=15, ipady=15,side='left')
+
+    label5 = tkinter.Label(window, text='Label5', background='yellow',fg='black')
+    label5.pack(ipadx=15, ipady=15,side='left')
+
+    label6 = tkinter.Label(window, text='Label6', background='green',fg='white')
+    label6.pack(ipadx=15, ipady=15,side='right')
+
+geometria_grid()
 window.mainloop()
 
